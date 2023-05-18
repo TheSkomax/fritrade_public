b06f7fb2e60834df4b779e2d06251641f0ee5fad
TheSkomax <theskomax@protonmail.com>
2023-05-18 17:55:33 +0200

    funkcia open_trade

--- trader_atr_sf/email_trader_forex/api_communicator.py
+++ trader_atr_sf/email_trader_forex/api_communicator.py
@@ -132,7 +132,7 @@ class XtbApi:
         cmd = self.xtb_client.execute(com)
         return cmd["returnData"]["margin"]
 
-    def get_symbol(self, symbol):
+    def get_symbol_specs(self, symbol):
         time.sleep(.1)
         com = {
             "command": "getSymbol",
@@ -140,10 +140,10 @@ class XtbApi:
                 "symbol": symbol
             }
         }
-        cmd = self.xtb_client.execute(com)
-        return cmd["returnData"]
+        execute_com = self.xtb_client.execute(com)
+        return execute_com["returnData"]
 
-    def open_buy_order(self, symbol, volume, timeframe, sl, tp):
+    def open_buy_position(self, symbol, volume, timeframe, sl, tp):
         transaction_info = {
             "cmd": 0,
             "symbol": symbol,
@@ -178,7 +178,7 @@ class XtbApi:
                 "message": confirm[1],
                 "margin": margin}
 
-    def open_sell_order(self, symbol, volume, timeframe, sl, tp):
+    def open_sell_position(self, symbol, volume, timeframe, sl, tp):
         transaction_info = {
             "cmd": 1,
             "symbol": symbol,
@@ -235,115 +235,42 @@ class XtbApi:
         cmd = self.xtb_client.execute(com)
         return cmd["returnData"]
 
-    def sl_tp_override(self, symbol, timeframe, trade_type):
-        symbol_specs = self.get_symbol(symbol)
-        ask = symbol_specs["ask"]
-        bid = symbol_specs["bid"]
-
-        if trade_type == "buy":
-            q = 'select sl from tradeData where symbol = "US500" and timeframe = "1h"'
-            fri_trade_cursor.execute(q)
-            sl_val = fri_trade_cursor.fetchone()[0]
-            sl = round((ask + sl_val), 1)  # sl_val je zaporna hodnota, preto +
-            # print(f"ask {ask} + sl_val {sl_val} = sl {sl}")
-
-            q = 'select tp from tradeData where symbol = "US500" and timeframe = "1h"'
-            fri_trade_cursor.execute(q)
-            tp_val = fri_trade_cursor.fetchone()[0]
-            tp = round((ask + tp_val), 1)
-            # print(f"ask {ask} + tp_val {tp_val} = tp {tp}")
-
-            return sl, tp
-
-        elif trade_type == "sell":
-            q = 'select sl from tradeData where symbol = "US500" and timeframe = "1h"'
-            fri_trade_cursor.execute(q)
-            sl_val = fri_trade_cursor.fetchone()[0]
-            sl = round((bid - sl_val), 1)  # sl_val je zaporna hodnota
-
-            q = 'select tp from tradeData where symbol = "US500" and timeframe = "1h"'
-            fri_trade_cursor.execute(q)
-            tp_val = fri_trade_cursor.fetchone()[0]
-            tp = round((bid - tp_val), 1)
-            return sl, tp
-
 
 xtb = XtbApi()
 
 
-def open_trade(chart_name, indicator, operation, takeprofit_pips, stoploss_pips):
+def open_trade(operation, price_close, takeprofit_pips, stoploss_pips, symbol, timeframe):
     xtb.login()
-    symbol, timeframe = chart_name.split("_")
-
-    symbol_specs = xtb.get_symbol(symbol)
+    lots = 0.09
+    indicator = "SF Strong"
+    symbol_specs = xtb.get_symbol_specs(symbol)
     ask = symbol_specs["ask"]
     bid = symbol_specs["bid"]
-    # mini_logger.info("=== TRADE PREPARATIONS STARTED ===")
-
-    q = f"""SELECT lots from fri_trade.tradeData where symbol = '{symbol}' and timeframe = '{timeframe}'"""
-    fri_trade_cursor.execute(q)
-    lots = fri_trade_cursor.fetchone()[0]
-    print(f"Lots in database for {symbol} {timeframe}: {lots}")
-
-    # account_properties = xtb.get_margin_level()
-    # balance = account_properties["balance"]
-    # margin_actual = account_properties["margin_needed_for_new_trade"]
-    # currency = account_properties["currency"]
-    # credit = account_properties["credit"]
-    # equity = account_properties["equity"]
-    # margin_free = account_properties["margin_free"]
-    # margin_level = account_properties["margin_level"]
-    # print("margin_actual", margin_actual, "Eur", "\nmargin_level", margin_level, "%")
 
     if operation == "buy":
-        if symbol == "US500":
-            takeprofit_price = round((ask + float(takeprofit_pips)), 1)
-            stoploss_price = round((ask - float(stoploss_pips)), 1)
-        elif symbol == "EURCHF":
-            takeprofit_price = round((ask + float(takeprofit_pips)), 5)
-            stoploss_price = round((ask - float(stoploss_pips)), 5)
-        else:
-            raise ValueError("Unknown symbol!")
+        takeprofit_price = round((ask + float(takeprofit_pips)), 5)
+        stoploss_price =   round((ask - float(stoploss_pips)), 5)
 
-        transaction_data = xtb.open_buy_order(symbol=symbol,
-                                              volume=lots,
-                                              timeframe=timeframe,
-                                              sl=stoploss_price,
-                                              tp=takeprofit_price)
+        transaction_data = xtb.open_buy_position(symbol=symbol, volume=lots, timeframe=timeframe,
+                                                 sl=stoploss_price, tp=takeprofit_price)
+    elif operation == "sell":
+        takeprofit_price = round((bid - float(takeprofit_pips)), 5)
+        stoploss_price =   round((bid + float(stoploss_pips)), 5)
 
-    else:  # elif operation == "sell":
-        if symbol == "US500":
-            stoploss_price = round((bid + float(stoploss_pips)), 1)
-            takeprofit_price = round((bid - float(takeprofit_pips)), 1)
-        elif symbol == "EURCHF":
-            stoploss_price = round((bid + float(stoploss_pips)), 5)
-            takeprofit_price = round((bid - float(takeprofit_pips)), 5)
-        else:
-            raise ValueError("Unknown symbol!")
-
-        transaction_data = xtb.open_sell_order(symbol=symbol,
-                                               volume=lots,
-                                               timeframe=timeframe,
-                                               sl=stoploss_price,
-                                               tp=takeprofit_price)
+        transaction_data = xtb.open_sell_position(symbol=symbol, volume=lots, timeframe=timeframe,
+                                                  sl=stoploss_price, tp=takeprofit_price)
 
     ordernum = transaction_data["order"]
     sent = transaction_data["sent"]
     opened = transaction_data["opened"]
     message = transaction_data["message"]
-    margin_needed_for_new_trade = transaction_data["margin"]
-
-    out = f"TRANSACTION STATS - ORDER: \n\nOrder number: {ordernum}\nType: {operation}\nSymbol: {symbol} {timeframe}\nLots: {lots}    Margin: {margin_needed_for_new_trade}\nSent: {sent}\n\nOpened: {opened}\nMessage: {message}"
-    print(out)
+    margin_required = transaction_data["margin"]
 
     if opened:
-        out = f"{symbol} {timeframe} Order {ordernum} - TRADE OPENED!"
-        # mini_logger.warning(out)
-        print(out)
+        opened_message_status = f"{symbol} {timeframe} Order number {ordernum} - TRADE OPENED!"
     else:
-        out = f"{symbol} {timeframe} Order {ordernum} TRADE DENIED! - Reason: {message}"
-        # mini_logger.error(out)
-        print(out)
+        opened_message_status = f"{symbol} {timeframe} Order number {ordernum} - TRADE DENIED! - Reason: {message}"
+    print(opened_message_status)
 
     allopenedtrades = xtb.get_all_opened_only_positions()
     for opened_trade in allopenedtrades:
@@ -352,15 +279,13 @@ def open_trade(chart_name, indicator, operation, takeprofit_pips, stoploss_pips)
             timeframe = opened_trade["customComment"]
             symbol = opened_trade["symbol"]
 
-            q = f"""insert into fri_trade.positions (date, time, positionnum, ordernum, symbol, ordertype, lots, margin,
-                     conditionTriggered, timeframe, sent, opened, reason, protected) VALUES('{date_now()}',
+            q = f"""insert into fri_trade.positions_forex (date, time, positionnum, ordernum, symbol, ordertype, lots,
+                     margin, conditionTriggered, timeframe, sent, opened, reason, protected) VALUES('{date_now()}',
                      '{time_now_hms()}', '{positionnum}', '{ordernum}', '{symbol}', '{operation}', '{lots}',
-                     '{margin_needed_for_new_trade}', '{indicator}', '{timeframe}', '{sent}', '{opened}',
-                     '{message}', False)"""
+                     '{margin_required}', '{indicator}', '{timeframe}', '{sent}', '{opened}', '{message}', False)"""
             fri_trade_cursor.execute(q)
-
     xtb.logout()
 
 
-open_trade(chart_name=argv[1], indicator=argv[2], operation=argv[3],
-           takeprofit_pips=argv[4], stoploss_pips=argv[5])
+# open_trade(operation=argv[1], price_close=argv[2], takeprofit_pips=argv[3], stoploss_pips=argv[4], symbol=argv[5],
+#            timeframe=argv[6])
